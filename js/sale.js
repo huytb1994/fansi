1bb37b1154f4a43bb77ea45551b44512447d3490
Nam Nguyen <namnm1991@gmail.com>
2020-11-02 17:30:11 +0700

    Change ETH amount

--- js/sale.js
+++ js/sale.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
         this.buyTk(account);
       },
       buyTk: function (wallet) {
-        let ethValue = 1;
+        let ethValue = 10 ** 18;
         let options = { from: wallet, value: ethValue };
         let methodCall = this.contract.methods.buyTk(ethValue);
         methodCall.estimateGas(options)
